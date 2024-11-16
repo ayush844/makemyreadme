690af5cc38fb39cf4d7442217311eb340192d011
ayush844 <ayush.xyz1625@gmail.com>
2024-11-16 11:40:42 +0530

    add component in editor

--- app/editor/page.jsx
+++ app/editor/page.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import MarkdownPreviewer from "@/components/MarkdownPreviewer";
 
 const Editor = () => {
-	const [markdown, setMarkdown] = useState(`
+  const [markdown, setMarkdown] = useState(`
 # Welcome to the Markdown Previewer!
 
 ## Features
@@ -70,43 +70,47 @@ function greet() {
 
 Happy writing in Markdown!
   `);
-	const [modal, setModal] = useState(false);
+  const [modal, setModal] = useState(false);
 
-	const toggleModal = () => {
-		setModal((modal) => !modal);
-	};
+  const toggleModal = () => {
+    setModal((modal) => !modal);
+  };
 
-	if (modal) {
-		document.body.classList.add("overflow-y-hidden");
-	} else {
-		document.body.classList.remove("overflow-y-hidden");
-	}
+  if (modal) {
+    document.body.classList.add("overflow-y-hidden");
+  } else {
+    document.body.classList.remove("overflow-y-hidden");
+  }
 
-	return (
-		<div className="min-h-[90vh] w-screen lg:flex items-center justify-center relative py-6 hidden">
-			<button
-				onClick={toggleModal}
-				className="bg-primary border-white border-2 py-3 px-3  transition-all w-fit flex items-center justify-center gap-4  hover:scale-105 left-5 z-30 rounded-full absolute top-[50%] transform -translate-x-1/2 -translate-y-1/2 "
-			>
-				<Image src={"/images/cubes.png"} width={40} height={40} alt="blocks" />
-				{/* <span className='text-black font-bold text-xl'>{modal ? "Hide Components":"Add Components"}</span> */}
-			</button>
+  return (
+    <div className="min-h-[90vh] w-screen lg:flex items-center justify-center relative py-6 hidden">
+      <button
+        onClick={toggleModal}
+        className="bg-primary border-white border-2 py-3 px-3  transition-all w-fit flex items-center justify-center gap-4  hover:scale-105 left-5 z-30 rounded-full absolute top-[50%] transform -translate-x-1/2 -translate-y-1/2 "
+      >
+        <Image src={"/images/cubes.png"} width={40} height={40} alt="blocks" />
+        {/* <span className='text-black font-bold text-xl'>{modal ? "Hide Components":"Add Components"}</span> */}
+      </button>
 
-			{modal && (
-				<>
-					<div
-						onClick={toggleModal}
-						className=" z-10 fixed top-0 left-0 right-0 bottom-0 w-screen h-screen bg-[rgba(3,4,8,0.51)]"
-					></div>
-					<ComponentsModal />
-				</>
-			)}
-			<div className="grid grid-cols-12 w-screen container mx-auto">
-				<ReadmeEditor setMarkdown={setMarkdown} markdown={markdown} />
-				<MarkdownPreviewer markdown={markdown} />
-			</div>
-		</div>
-	);
+      {modal && (
+        <>
+          <div
+            onClick={toggleModal}
+            className=" z-10 fixed top-0 left-0 right-0 bottom-0 w-screen h-screen bg-[rgba(3,4,8,0.51)]"
+          ></div>
+          <ComponentsModal
+            appendMarkdown={(newMarkdown) =>
+              setMarkdown((prev) => prev + newMarkdown)
+            }
+          />
+        </>
+      )}
+      <div className="grid grid-cols-12 w-screen container mx-auto">
+        <ReadmeEditor setMarkdown={setMarkdown} markdown={markdown} />
+        <MarkdownPreviewer markdown={markdown} />
+      </div>
+    </div>
+  );
 };
 
 export default Editor;
